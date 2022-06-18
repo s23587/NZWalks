0c1d2dfb421e8580da4c50948a7e27ca36994363
s23587 <s23587@pjwstk.edu.pl>
2022-06-18 18:55:14 +0200

    Koniec sekcji 2

---
+++ NZWalks/NZWalks.API/Models/Domain/Walk.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NZWalks.API.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double length { get; set; }
+        public Guid RegionId { get; set; }
+        public Guid WalkDifficultyId { get; set; }
+
+        // Navigation properties
+
+        public Region Region { get; set; }
+        public WalkDifficulty WalkDifficulty { get; set; }
+
+    }
+}
--- NZWalks/NZWalks.API/Startup.cs
+++ NZWalks/NZWalks.API/Startup.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using NZWalks.API.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -31,6 +33,9 @@ namespace NZWalks.API
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "NZWalks.API", Version = "v1" });
+            });
+            services.AddDbContext<NZWalksDbContext>(options => {
+                options.UseSqlServer(Configuration.GetConnectionString("NZWalks"));
             });
         }
 
